cd106173e704252a39232a8fa20fb7d5ceb37902
FlukeyFiddler <robertovdmeer@gmail.com>
2021-03-01 16:39:57 +0100

    swirl assignments week 2 complete

--- firstFunctions.R
+++ firstFunctions.R
@@ -6,3 +6,20 @@ above <- function(x, n = 10) {
   use <- x > n
   x[use]
 }
+
+# Lexical Scoping
+"
+cube <- make.power(3)
+square <- make.power(2)
+cube(3)
+square(3)
+Check the free vars
+ls(environment(cube))
+get('n', environment(cube))
+"
+make.power <- function(n) {
+  pow <- function(x) {
+      x^n
+  }
+  pow
+}
