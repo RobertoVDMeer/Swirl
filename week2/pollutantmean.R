bf82d51e0278be030e6fd21d335e0a24f4bde0bf
FlukeyFiddler <robertovdmeer@gmail.com>
2021-03-03 04:44:04 +0100

    Programming ass Added pollutantmean

---
+++ week2/pollutantmean.R
@@ -0,0 +1,14 @@
+pollutantmean <- function(directory, pollutant, id = 1:332) {
+    readings <- c()
+    for(monitorId in id) {
+        filename <- generateMonitorFilename(directory, monitorId)
+        monitor <- read.csv(filename)
+        readings <- append(monitor[[pollutant]], readings)
+    }
+    mean(readings, trim = 0, na.rm = TRUE)
+}
+
+generateMonitorFilename <- function(directory, monitorId) {
+    fileName <- formatC(monitorId, width=3,  flag = "0")
+    paste(directory, "/", fileName, ".csv", sep = "")
+}
