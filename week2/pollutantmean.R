3a1bea3d32cc771c18b9053fcd59afe66c480101
FlukeyFiddler <robertovdmeer@gmail.com>
2021-03-03 06:44:00 +0100

    Added the 'complete' function for assignment week 2

--- week2/pollutantmean.R
+++ week2/pollutantmean.R
@@ -8,6 +8,21 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
     mean(readings, na.rm = TRUE)
 }
 
+complete <- function(directory, id = 1:332) {
+    complete <- data.frame(matrix(ncol = 2, nrow = 0))
+    
+    for(monitorId in id) {
+        filename <- generateMonitorFilename(directory, monitorId)
+        monitor <- read.csv(filename)
+        
+        completeCases <- sum(complete.cases(monitor))
+        
+        complete <- rbind.data.frame(complete, c(monitorId, completeCases))
+    } 
+    colnames(complete) <- c("id", "nobs")
+    complete
+}
+
 generateMonitorFilename <- function(directory, monitorId) {
     fileName <- formatC(monitorId, width=3,  flag = "0")
     paste(directory, "/", fileName, ".csv", sep = "")
